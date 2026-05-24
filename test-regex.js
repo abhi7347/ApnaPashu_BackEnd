1525999988848383fdbf7c63aa81844cd01652a8
Abhishek Tiwari <abhishektiwari@smartdata.net>
2026-05-24 13:47:03 +0530

    Remove generated documentation and temp files

--- test-regex.js
+++
@@ -1,8 +0,0 @@
-function testEmail(email) {
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    console.log(email + ": " + emailPattern.test(email));
-}
-
-testEmail('abhi@eza.co');
-testEmail('abhi@yopmail.comdsfdsf');
-testEmail('abhi@');
